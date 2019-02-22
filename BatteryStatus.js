b7b76133fcd32b3769ee72b42196d3ece7272730
Harold Espinoza <haroldesptru@gmail.com>
2019-02-21 20:48:08 -0600

    Añadido: evento que detecta cuando el cargador se conecta/desconecta

--- BatteryStatus.js
+++ BatteryStatus.js
@@ -1,6 +1,13 @@
 try{
   navigator.getBattery()
     .then(battery => {
+
+      // Detect charging state
+      battery.addEventListener('chargingchange', () => {
+        let charging = (battery.charging) ? 'conectado' : 'desconectado';
+        console.log(`Charging state: Cargador ${charging}`);
+      });
+
     });
 }catch (e) {
   console.error("Battery API not found");

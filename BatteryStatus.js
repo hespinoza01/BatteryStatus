a070a2a98d7d3d4623f3429d57c12d8af96a9026
Harold Espinoza <haroldesptru@gmail.com>
2019-02-21 21:11:37 -0600

    Añadido: eventos pata calcular el tiempo estimado de carga/descarga de la batería.

--- BatteryStatus.js
+++ BatteryStatus.js
@@ -1,16 +1,28 @@
 try{
   navigator.getBattery()
     .then(battery => {
+      let chargingBattery = false;
 
       // Detect charging state
       battery.addEventListener('chargingchange', () => {
-        let charging = (battery.charging) ? 'conectado' : 'desconectado';
+        chargingBattery = battery.charging;
+        let charging = (chargingBattery) ? 'conectado' : 'desconectado';
         console.log(`Charging state: Cargador ${charging}`);
       });
 
       // Detect battery level change
       battery.addEventListener('levelchange', () => {
         console.log(`Battery level: ${battery.level * 100}%`);
+      });
+
+      // Calc battery charging time change
+      battery.addEventListener('chargingtimechange', () => {
+        if(chargingBattery) console.log(`Tiempo estimado para carga completa de la batería: ${battery.chargingTime} segundos`);
+      });
+
+      // Calc battery discharging time change
+      battery.addEventListener('dischargingtimechange', () => {
+        if(!chargingBattery) console.log(`Duración estimada de la batería: ${battery.dischargingTime} segundos`);
       });
 
     });
